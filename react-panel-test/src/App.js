2fff20f0ddc9d218166d90aeea6f0d8f1989cf8a
afridizx <afridizx@dukes.jmu.edu>
2019-10-10 00:01:01 -0400

    initial working card

--- react-panel-test/src/App.js
+++ react-panel-test/src/App.js
@@ -1,30 +1,12 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 // import logo from './logo.svg'
 import './App.css'
-import Card from './components/Card'
+import { Card } from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        <Card />
-
-        
-      {/* </header> */}
-    </div>
+    <Card bg="dark" text="white" style={{ width: '25rem', height: '25rem' }} />
   )
 }
 
