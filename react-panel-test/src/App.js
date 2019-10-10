17588c35bb542df7083ef831073c95066e24fac4
afridizx <afridizx@dukes.jmu.edu>
2019-10-09 20:25:53 -0400

    card component testing

--- react-panel-test/src/App.js
+++ react-panel-test/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import logo from './logo.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import logo from './logo.svg'
 import './App.css'
 import Card from './components/Card'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,11 +19,11 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
         <Card />
 
         
-      </header>
+      {/* </header> */}
     </div>
   )
 }
--- react-panel-test/src/components/Card.js
+++ react-panel-test/src/components/Card.js
@@ -3,7 +3,13 @@ import React from 'react';
 
 class Card extends React.Component {
   render() {
-    return <h2>Component works, son.</h2>;
+    // return <h2>Component works, son.</h2>;
+    return(
+      <Card bg="secondary" text="white" style={{ width: '25rem', height: '25rem' }}>
+        <Card.Header>Header</Card.Header>
+        <Card.Body />
+      </Card>
+    )
   }
 
 }
