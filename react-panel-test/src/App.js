4e2d067c4c98ca4516155afaef4155457f3766a7
afridizx <afridizx@dukes.jmu.edu>
2019-10-10 21:33:05 -0400

    setup for passenger and front camera feeds

--- react-panel-test/src/App.js
+++ react-panel-test/src/App.js
@@ -1,37 +1,39 @@
 import React from 'react'
 import { Card, Row, Col } from 'react-bootstrap'
 import './App.css'
-
+// import '../public/logo192'
 function App() {
   return (
-      <div>
-        <Row
-          style={{
-            marginTop: 10,
-            marginLeft: 10
-          }}
-        >
-          <Col xs={0}>
-            <Card
-              bg="dark"
-              text="white"
-              style={{ width: '32rem', height: '18rem' }}
-            >
-              <Card.Header>Passenger Feed</Card.Header>
-            </Card>
-          </Col>
+    <div>
+      <Row
+        style={{
+          marginTop: 10,
+          marginLeft: 10
+        }}
+      >
+        <Col xs={0}>
+          <Card
+            bg="dark"
+            text="white"
+            style={{ width: '32rem', height: '18rem' }}
+          >
+            <Card.Body>Passenger Camera Feed</Card.Body>
+            <Card.Img variant="top" src={require('./logo.svg')} />
+          </Card>
+        </Col>
 
-          <Col>
-            <Card
-              bg="dark"
-              text="white"
-              style={{ width: '32rem', height: '18rem' }}
-            >
-              <Card.Header>Front Camera Feed</Card.Header>
-            </Card>
-          </Col>
-        </Row>
-      </div>
+        <Col>
+          <Card
+            bg="dark"
+            text="white"
+            style={{ width: '32rem', height: '18rem' }}
+          >
+            <Card.Body>Front Camera Feed</Card.Body>
+            <Card.Img variant="top" src={require('./logo.svg')} />
+          </Card>
+        </Col>
+      </Row>
+    </div>
   )
 }
 
