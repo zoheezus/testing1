7deeb9d1bebed12fdcbded661f08c5cd69b3513e
afridizx <afridizx@dukes.jmu.edu>
2019-10-14 10:18:35 -0400

    fix height

--- react-panel-test/src/App.js
+++ react-panel-test/src/App.js
@@ -15,8 +15,9 @@ function App() {
     <div
       style={{
         backgroundColor: '#232931',
-        height: '100vh',
-        backgroundPosition: 'center'
+        width: '100%',
+        height: '100%',
+        // backgroundPosition: 'center'
       }}
     >
       <Row
@@ -29,7 +30,7 @@ function App() {
             <Card
               bg="dark"
               text="white"
-              style={{ width: '30rem', height: '30rem' }}
+              style={{ width: '25rem', height: '25rem' }}
             >
               <Card.Body as="h5">Passenger Camera Feed</Card.Body>
               <Card.Img variant="top" src={require('./logo.svg')} />
@@ -40,7 +41,7 @@ function App() {
             <Card
               bg="dark"
               text="white"
-              style={{ width: '30rem', height: '30rem' }}
+              style={{ width: '25rem', height: '25rem' }}
             >
               <Card.Body as="h5">Front Camera Feed</Card.Body>
               <Card.Img variant="top" src={require('./logo.svg')} />
@@ -51,7 +52,7 @@ function App() {
             <Card
               bg="dark"
               text="white"
-              style={{ width: '40rem', height: '30rem' }}
+              style={{ width: '40rem', height: '25rem' }}
             >
               <Card.Header as="h5">Logs</Card.Header> {/* mock data */}
               <Card.Body>
@@ -85,7 +86,7 @@ function App() {
           <Col xs={0}>
             <Card
               text="white"
-              style={{ backgroundColor: '#393e46', width: '30rem' }}
+              style={{ backgroundColor: '#393e46', width: '25rem' }}
             >
               <Card.Header as="h5" style={{ backgroundColor: '#0ecca3' }}>
                 In Queue
@@ -107,7 +108,7 @@ function App() {
           <Col xs={0}>
             <Card
               text="white"
-              style={{ backgroundColor: '#393e46', width: '30rem' }}
+              style={{ backgroundColor: '#393e46', width: '25rem' }}
             >
               <Card.Header as="h5" style={{ backgroundColor: '#4ecca3' }}>
                 Message Cart
@@ -130,7 +131,7 @@ function App() {
               style={{
                 backgroundColor: '#393e46',
                 marginTop: '10px',
-                width: '30rem'
+                width: '25rem'
               }}
             >
               <Card.Header as="h5" style={{ backgroundColor: '#4ecca3' }}>
@@ -150,8 +151,8 @@ function App() {
             </Card>
           </Col>
 
-          <Col xs={0}>
-            <Card style={{ backgroundColor: '#393e46', height: '15rem' }}>
+          <Col>
+            <Card style={{ backgroundColor: '#393e46', height: '15rem', width: '100%' }}>
               <Button
                 as="input"
                 type="button"
@@ -166,8 +167,8 @@ function App() {
               />
             </Card>
           </Col>
-          <Col xs={0}>
-            <Card style={{ backgroundColor: '#393e46', height: '15rem' }}>
+          <Col>
+            <Card style={{ backgroundColor: '#393e46', height: '15rem', width: '100%' }}>
               <Button
                 as="input"
                 type="button"
